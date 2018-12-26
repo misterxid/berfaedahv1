9537c2c80a628e77aa9fa212f33cf85b67e11fa0
Mr_XiD <44462886+safri1234@users.noreply.github.com>
2018-12-26 11:24:53 +0700

    Create zon.sh

---
+++ zon.sh
@@ -0,0 +1,79 @@
+#mau reqod
+#malulah dengan dirimu
+
+figlet MR_XID  | lolcat
+
+echo                    " [======<>======<>======<>======]"|lolcat
+echo                    " [Author   :  MR_XiD            ]"|lolcat
+echo                    " [Team     : JATIM BLACKHAT     ]"|lolcat
+echo                    " [Note     : Jangan reqode ya   ]"|lolcat
+echo                    " [Thansk to: All member         ]"|lolcat
+echo                    " [thanks to: I T A              ]"|lolcat
+echo                    " [Note2    : Copyright©MR_XID   ]"|lolcat
+echo                    " [======<>======<>======<>======]"|lolcat
+
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>" | lolcat
+echo    " 1. TEMBAK CEWE "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>" | lolcat
+echo    " 2. SANTET MANTAN "   
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>" | lolcat
+echo    " 3. HAMMER DDOS "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    "╭─[PILIH NOMERNYA]"
+read -p "╰─root@./MR_XID=" saf;
+
+if [ $saf = 1 ] || [ $saf = 1 ]
+then
+echo    "======================"|lolcat
+echo    " 1. cara tembak "
+echo    "======================"|lolcat
+echo    " 2. cara tembak 2 "
+echo    "======================" | lolcat
+echo    "╭─[PILIH NOMERNYA]"
+read -p "╰─root@./MR_XID=" fri;
+fi
+if [ $fri = 1 ] || [ $fri = 1 ]
+then
+echo "-CARA MENEMBAK CEWEK YA TONG:v-" | lolcat
+sleep 1
+echo " (1).PERTAMA KAMU HARUS PUNYA DUIT" | lolcat
+sleep 1
+echo " (2).KALO KAGA YAUDAH TANDANYA ELO GAGAL NEMBAK CEWE  :V" | lolcat 
+sleep 1
+echo " (3).JOMBLO HAPPY AJA TOD 😂 " | lolcat
+sleep 1
+echo " (4).TERAKHIR. KAMU BERIBADAH  KEPADA ALLAH SWT " | lolcat
+sleep 1
+fi 
+if [ $fri = 2 ] || [ $fri = 2 ]
+then
+echo " 1. cara menembak cewe "
+sleep 2
+echo " 2. lu apa udah kerja "
+sleep 1
+echo " 3. kerja aja belum mau dapet cewe"
+echo " 4. yaudah cari kerja dulu baru nanti cari cewe ok 😊"
+fi
+if [ $saf = 2 ] || [ $saf = 2 ]
+then 
+pkg install git
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sleep 1
+sh LITESPAM.sh
+echo "langsung santet mantan :v" | lolcat
+fi
+if [ $saf = 3 ] || [ $saf = 3 ]
+then 
+pkg update 
+pkg upgrade 
+pkg install python 
+pkg install git 
+git clone https://github.com/cyweb/hammer 
+cd hammer 
+chmod 7777 hammer.py
+sleep 1
+python hammer.py 
+fi
+exit
+fi
